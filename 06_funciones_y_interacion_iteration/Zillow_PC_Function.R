78e7403aea3632460b5c108bc3894ef68c1707c5
David Sosa Olea <122306050+davidsosaolea@users.noreply.github.com>
2024-01-01 21:28:53 -0500

    ggplot y Plotly

---
+++ 06_funciones_y_interacion_iteration/Zillow_PC_Function.R
@@ -0,0 +1,51 @@
+## ____________________________________
+##
+## Nombre del script: Zillow_PC_Function.R
+##
+## Propósito del script: Data Manipulation
+##
+## Requisitos previos:
+##   * funciones_script_de_importacion_de_datos.R
+##
+## Parámetros de la función:
+##   * Y1
+##   * Y2
+##   * show_rows
+## 
+##
+## Códigos de retorno:
+## 0 - Éxito: 
+## 1 - Error: 
+## 2 - Error: 
+## 3 - Error: 
+##
+## Autor: BIDDSA
+## Fecha de creación: 2023-09-11
+## Derechos de autor: (c) BIDDSA, 2023
+## Correo electrónico: <email>@BIDDSA.com
+##________________________________________________________________________
+##> Notas:
+##> 
+##> 
+##________________________________________________________________________
+## HISTORIA DE MODIFICACIÓN
+## 06AGO2023 persona0
+## Cambios: Versión inicial.
+##
+## 06AGO2023 persona1
+## Cambios: - Cambios xxx
+##
+## 07AGO2023 persona2
+## Cambios: - Se eliminó xxx
+##
+##________________________________________________________________________
+
+# Paquetes 
+library(readxl)
+library(tidyverse) 
+library(lubridate)
+library(fs)
+
+
+
+

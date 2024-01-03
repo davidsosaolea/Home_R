4aa6dc704e78d37dba7ac6f24bf9299be19f1c99
David Sosa Olea <122306050+davidsosaolea@users.noreply.github.com>
2024-01-02 21:40:35 -0500

    Visualizaciones Dinámicas y Multi-Panel con ggplot2 y Plotly

---
+++ 05_Graficos_Graphics/EcoGraphHomeUnemploy.R
@@ -0,0 +1,112 @@
+## ____________________________________
+##
+## Nombre del script: EcoGraphHomeUnemploy.r
+##
+## Propósito del script: Visualization de Datos 
+##
+## Requisitos previos:
+## *  Zillow_PC_Function.R
+## 
+##
+## Parámetros de la función:
+##   * Y1
+##   * Y2
+##   * show_rows:
+##
+## 
+## Resultados:
+##
+## Códigos de retorno: NA
+##
+## Autor: BIDDSA
+## Fecha de creación: 2023-09-11
+## Derechos de autor: (c) BIDDSA, 2023
+## Correo electrónico: <email>@BIDDSA.com
+##________________________________________________________________________
+##> Notas:
+##> 
+##> 
+##________________________________________________________________________
+## HISTORIA DE MODIFICACIÓN
+## 06AGO2023 persona0
+## Cambios: Versión inicial.
+##
+## 06AGO2023 persona1
+## Cambios: - Cambios xxx
+##
+## 07AGO2023 persona2
+## Cambios: - Se eliminó xxx
+##
+##________________________________________________________________________
+
+# install.packages()
+# Bibliotecas ----
+library(readxl)
+library(tidyverse)
+library(naniar)
+library(lubridate)
+library(stringr)
+library(fs)
+library(scales)
+library(ggplot2)
+library(plotly)
+library(tidyquant)
+library(ggrepel)
+
+
+source("06_funciones_y_interacion_iteration/Zillow_PC_Function.R")
+source("06_funciones_y_interacion_iteration/funciones_de_importacion.R")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

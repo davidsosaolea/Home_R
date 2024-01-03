f62ec3a71834064dc2164ea0c3d1d26c84ec0954
David Sosa Olea <122306050+davidsosaolea@users.noreply.github.com>
2024-01-02 21:20:18 -0500

    Estructura de Datos para ggplot y Plotly

--- 06_funciones_y_interacion_iteration/Zillow_PC_Function.R
+++ 06_funciones_y_interacion_iteration/Zillow_PC_Function.R
@@ -46,6 +46,42 @@ library(tidyverse)
 library(lubridate)
 library(fs)
 
+getTopCitiesByCAGR <- function (Y1, Y2, show_rows){
+    source("06_funciones_y_interacion_iteration/funciones_de_importacion.R")
 
+# Importacion de datos        
+    ZMI_Data_Prep <- data_import("ZMI_data_pre")
+    
+# Data prep
+    
+   CAGR <- ZMI_Data_Prep |>
+        filter (Year %in% c(Y1, Y2)) |>
+        group_by(City_zip) |>
+        mutate (Beginning_Value = Z_Home_Value_Index[Year == Y1],
+                Ending_Value = Z_Home_Value_Index[Year == Y2]) |>
+        ungroup() |>
+        select (City_zip, Beginning_Value, Ending_Value) |>
+        mutate(timeperiod = str_c(Y1, Y2, sep = "-")) |>
+        distinct()
+   
+# calculando CAGR
+# CAGR se establece para ser calculado en base a Y1 y Y2
+   CAGR <- CAGR |>
+       mutate (Number_of_Years = Y2 - Y1,
+               CAGR = ((Ending_Value / Beginning_Value)^(1/ Number_of_Years)) - 1)|>
+       arrange (desc(CAGR))
 
-
+# Top Cities
+   if(show_rows == "all") {
+       
+       show_rows <- CAGR |> nrow()
+       CAGR <- CAGR[1:show_rows,]
+       return(CAGR)
+   }
+   if (is.numeric(show_rows) == TRUE) {
+       CAGR <- CAGR [1: show_rows,]
+       return(CAGR)
+       
+   }else
+       print("Ingrese 'all' o un valor numerico")
+}

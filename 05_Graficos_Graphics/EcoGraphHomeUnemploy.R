c4bdf2eafa1f82a0b0caa77e759f6f3244206553
David Sosa Olea <122306050+davidsosaolea@users.noreply.github.com>
2024-01-07 21:13:24 -0500

    plotly

--- 05_Graficos_Graphics/EcoGraphHomeUnemploy.R
+++ 05_Graficos_Graphics/EcoGraphHomeUnemploy.R
@@ -334,8 +334,8 @@ homev_vs_Job_growth_III <- Employment_C |>
 homev_vs_Job_growth_III <- homev_vs_Job_growth_III |>
     mutate (Home_Value_Index_PC = `Home Value Index` |> scales:: percent (accuracy = 0.01)) |> 
     mutate (Employment_PC       =  PC_YoY |> scales::percent (accuracy = 0.01)) |>
-    mutate (label_text1         = str_glue("Employment: (Employment_PC}%")) |>
-    mutate (label_text2         = str_glue("Home Value Index: {Home_Value_Index_PC} %")) |>
+    mutate (label_text1         = str_glue("Employment: (Employment_PC}")) |>
+    mutate (label_text2         = str_glue("Home Value Index: {Home_Value_Index_PC}")) |>
     mutate(label_text3          = str_glue("E: {Employment_PC}%")) |>
     mutate(label_text4          = str_glue("H: {Home_Value_Index_PC}%"))
 
